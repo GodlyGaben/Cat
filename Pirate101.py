8457c0a28efefe203a5b118fdad4a31c5030f924
GodlyGaben <abk@hundzor.org>
2018-10-02 18:50:03 -0500

    Add files via upload

--- Pirate101.py
+++ Pirate101.py
@@ -1,18 +1,39 @@
 import random
+from tkinter import *
 
 class pirate():
-    firstname = "Amory"
-    nickname = "Squealer"
-    lastname = "Addington"
-    fullname = "Amory 'Squealer' Addington"
 
     fullList = ["Amory 'Squealer' Addington", "Mr. Sir", "Twinkie", "Twinkletoes" ,  "Stereotypical Irish Man"]
     def __init__ (self, fullname):
         self.fullname = fullname
 
     def CreateName(self):
-        x = random.randint(0, (self.fullList) - 1)
+        x = random.randint(0, len(self.fullList) - 1)
         return self.fullList[x]
 
-mypirate = pirate("Andrew")
-print(mypirate.CreateName())
+#mypirate = pirate("Andrew")
+#print(mypirate.CreateName())
+
+root = Tk()
+myfont = "SegoeScript 14"
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+banner = PhotoImage(file="paper.gif")
+banner = banner.subsample(2,2)
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+title = Label(root, text="A life of scurvy awaits ye!", font="Script 40")
+
+flabel = Label(root, text="Fullname", font=myfont)
+
+bshow = Button(root, text="Generate a new name.", font=myfont)
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+title.grid(row=0, column=0, columnspan=2)
+
+flabel.grid(row=1, column=2)
+
+ftext = Entry (root, font=myfont)
+
+bshow.grid(row = 1, column = 5)
+
+ftext.grid(row = 2, column = 2)
+
+root.mainloop ()
