b09f60b7031ca04e8b23042f63ae7d4d17345258
GodlyGaben <abk@hundzor.org>
2018-10-23 19:24:04 -0500

    Add files via upload

--- asdfghjjhgfds.py
+++ asdfghjjhgfds.py
@@ -1,29 +1,16 @@
-class Pirate:
-    name = " "
-    ship = " "
-    badass = True
-    real = True
-
-    def getDict(self):
-        d = {"name":  self.name, "ship": self.ship, "real": self.real, "badass": self.badass}
-        return d
-    def loadFromDict (self, d):
-        self.name = d["name"]
-        self.ship = d["ship"]
-        self.badass = d["badass"]
-        self.real = d["real"]
-class FileManager:
-    path = "pirateDB.json"
-
-    def writeToFile(self, idNm, obj):
-        try:
-            f = open(self.patth, "r")
-            d = json.load(f)
-            f.close()
-        except:
-            d = ()
-            d[idNum] = obj
-            f = open(self.path, "w")
-            json.dump(d, f)
-            f.close ()
-        
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/Users/powerupta18/Desktop/Documents/asdfghjjhgfds.py ======
+Traceback (most recent call last):
+  File "C:/Users/powerupta18/Desktop/Documents/asdfghjjhgfds.py", line 48, in <module>
+    Dropdown = OptionMenu(root, optionstring, "True", "False")
+NameError: name 'optionstring' is not defined
+>>> 
+====== RESTART: C:/Users/powerupta18/Desktop/Documents/asdfghjjhgfds.py ======
+Traceback (most recent call last):
+  File "C:/Users/powerupta18/Desktop/Documents/asdfghjjhgfds.py", line 49, in <module>
+    dropdown.config(font = "Arial 12", width="10")
+NameError: name 'dropdown' is not defined
+>>> 
+====== RESTART: C:/Users/powerupta18/Desktop/Documents/asdfghjjhgfds.py ======
